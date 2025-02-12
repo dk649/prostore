62b136561af530b55a9ab3276d1dd613da564aec
dk649 <58308212+dk649@users.noreply.github.com>
2025-02-12 15:03:38 +0000

    update payments

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,6 +3,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  paymentMethodSchema,
 } from "../validators";
 
 import { auth, signIn, signOut } from "@/auth";
@@ -130,5 +131,34 @@ export async function updateUserAddress(data: ShippingAddress) {
       success: false,
       nessage: formatError(error),
     };
+  }
+}
+
+// update users payment method
+
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
   }
 }
